2bb591e37eb45eb4a3cce4ac8334d865dcec7f04
louix.sm@gmail.com <Louiixx-h>
2023-04-05 20:51:28 -0300

    add elvis on movie's fields

--- app/src/main/java/br/com/luishenrique/moviesbrasil/home/models/MovieMapper.kt
+++ app/src/main/java/br/com/luishenrique/moviesbrasil/home/models/MovieMapper.kt
@@ -1,27 +1,20 @@
 package br.com.luishenrique.moviesbrasil.home.models
 
-import br.com.luishenrique.moviesbrasil.home.models.responses.GenreResponseVO
 import br.com.luishenrique.moviesbrasil.home.models.responses.MovieResponseVO
 import br.com.luishenrique.moviesbrasil.home.models.responses.ResultMovieResponseVO
 
 object MovieMapper {
 
-    fun transform(resultMovie: ResultMovieResponseVO) : ResultMovie {
-        return ResultMovie(
-            results = resultMovie.results.map { getMovie(it) }
-        )
-    }
-
-    private fun getMovie(movie: MovieResponseVO) = Movie(
-        id = movie.id,
-        title = movie.title,
-        voteAverage = movie.voteAverage,
-        posterPath = movie.posterPath,
-        backdropPath = movie.backdropPath,
-        genres = getGenres(movie.genres),
+    fun transform(resultMovie: ResultMovieResponseVO) = ResultMovie(
+        results = resultMovie.results?.map { getMovie(it) }.orEmpty()
     )
 
-    private fun getGenres(genres: List<GenreResponseVO>?) = genres?.map {
-        Genre(id = it.id, name = "")
-    }
+    private fun getMovie(movie: MovieResponseVO) = Movie(
+        id = movie.id ?: 0,
+        title = movie.title.orEmpty(),
+        voteAverage = movie.voteAverage ?: 0.0,
+        posterPath = movie.posterPath.orEmpty(),
+        backdropPath = movie.backdropPath.orEmpty(),
+        genres = movie.genres.orEmpty(),
+    )
 }
