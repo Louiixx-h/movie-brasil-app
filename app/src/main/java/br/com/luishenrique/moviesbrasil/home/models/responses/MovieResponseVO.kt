3249fff0dfbaac9763980e1ccb2d259466f7ed8c
louix.sm@gmail.com <Louiixx-h>
2023-04-05 20:48:59 -0300

    refactoring on responsesVO

--- app/src/main/java/br/com/luishenrique/moviesbrasil/home/models/responses/MovieResponseVO.kt
+++ app/src/main/java/br/com/luishenrique/moviesbrasil/home/models/responses/MovieResponseVO.kt
@@ -5,8 +5,8 @@ import com.google.gson.annotations.SerializedName
 data class MovieResponseVO(
     @SerializedName("id") val id: Int?,
     @SerializedName("title") val title: String?,
-    @SerializedName("vote_average") val voteAverage: String?,
+    @SerializedName("vote_average") val voteAverage: Double?,
     @SerializedName("poster_path") val posterPath: String?,
     @SerializedName("backdrop_path") val backdropPath: String?,
-    @SerializedName("genre_ids") val genres: List<GenreResponseVO>,
+    @SerializedName("genre_ids") val genres: List<Int>?,
 )
--- app/src/main/java/br/com/luishenrique/moviesbrasil/home/models/responses/ResultMovieResponseVO.kt
+++ app/src/main/java/br/com/luishenrique/moviesbrasil/home/models/responses/ResultMovieResponseVO.kt
@@ -3,5 +3,5 @@ package br.com.luishenrique.moviesbrasil.home.models.responses
 import com.google.gson.annotations.SerializedName
 
 data class ResultMovieResponseVO(
-    @SerializedName("results") val results: List<MovieResponseVO>
+    @SerializedName("results") val results: List<MovieResponseVO>?
 )
