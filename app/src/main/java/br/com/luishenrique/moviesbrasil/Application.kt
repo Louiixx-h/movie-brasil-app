8e643b7ec2fd44e848bba9bbafc50274cd5c435b
louix.sm@gmail.com <Louiixx-h>
2023-04-06 16:59:56 -0300

    start koin added on Application.kt

--- app/src/main/java/br/com/luishenrique/moviesbrasil/Application.kt
+++ app/src/main/java/br/com/luishenrique/moviesbrasil/Application.kt
@@ -1,11 +1,28 @@
 package br.com.luishenrique.moviesbrasil
 
 import br.com.luishenrique.moviesbrasil.favorites.service.StorageImpl
+import br.com.luishenrique.moviesbrasil.home.di.homeModule
+import br.com.luishenrique.moviesbrasil.service.api.di.serviceModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 class Application : android.app.Application() {
 
     override fun onCreate() {
         super.onCreate()
+
         StorageImpl.init(context = this)
+
+        startKoin{
+            androidLogger()
+            androidContext(this@Application)
+            modules(appModule())
+        }
     }
+
+    private fun appModule() = listOf(
+        serviceModule,
+        homeModule,
+    )
 }
