fbbc1593d8bd801de2f639ed56d187c71ce33f30
louix.sm@gmail.com <Louiixx-h>
2023-04-10 18:55:40 -0300

    koin configured to service

--- app/src/main/java/br/com/luishenrique/moviesbrasil/service/api/di/AppModule.kt
+++ app/src/main/java/br/com/luishenrique/moviesbrasil/service/api/di/AppModule.kt
@@ -3,17 +3,18 @@ package br.com.luishenrique.moviesbrasil.service.api.di
 import br.com.luishenrique.moviesbrasil.service.api.ApiService
 import br.com.luishenrique.moviesbrasil.utils.BASE_URL
 import org.koin.dsl.module
+import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 val serviceModule = module {
-    single { createService() }
+    single { provideService(BASE_URL, GsonConverterFactory.create()) }
 }
 
-private fun createService(): ApiService {
+private fun provideService(baseURL: String, factory: Converter.Factory): ApiService {
     val retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(baseURL)
+        .addConverterFactory(factory)
         .build()
 
     return retrofit.create(ApiService::class.java)
