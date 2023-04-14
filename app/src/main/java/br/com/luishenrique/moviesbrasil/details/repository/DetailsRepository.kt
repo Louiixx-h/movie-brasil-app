5712f2e8505cf370472151228f51df015bcf81a1
louix.sm@gmail.com <Louiixx-h>
2023-04-14 20:45:24 -0300

    addMovieToFavorites, removeMovieToFavorites, hasMovie added

--- app/src/main/java/br/com/luishenrique/moviesbrasil/details/repository/DetailsRepository.kt
+++ app/src/main/java/br/com/luishenrique/moviesbrasil/details/repository/DetailsRepository.kt
@@ -1,8 +1,12 @@
 package br.com.luishenrique.moviesbrasil.details.repository
 
+import br.com.luishenrique.moviesbrasil.details.models.MovieDetail
 import br.com.luishenrique.moviesbrasil.details.models.responses.MovieDetailsResponseVO
 import retrofit2.Response
 
 interface DetailsRepository {
     suspend fun getDetails(movieId: Int): Response<MovieDetailsResponseVO>
+    fun addMovieToFavorites(movie: MovieDetail): Boolean
+    fun removeMovieToFavorites(movie: MovieDetail): Boolean
+    fun hasMovie(movie: MovieDetail): Boolean
 }
--- app/src/main/java/br/com/luishenrique/moviesbrasil/details/repository/DetailsRepositoryImpl.kt
+++ app/src/main/java/br/com/luishenrique/moviesbrasil/details/repository/DetailsRepositoryImpl.kt
@@ -1,12 +1,29 @@
 package br.com.luishenrique.moviesbrasil.details.repository
 
+import br.com.luishenrique.moviesbrasil.details.models.MovieDetail
 import br.com.luishenrique.moviesbrasil.details.models.responses.MovieDetailsResponseVO
+import br.com.luishenrique.moviesbrasil.service.Storage
 import br.com.luishenrique.moviesbrasil.service.api.ApiService
 import retrofit2.Response
 
-class DetailsRepositoryImpl(private val service: ApiService): DetailsRepository {
+class DetailsRepositoryImpl(
+    private val service: ApiService,
+    private val storage: Storage
+): DetailsRepository {
 
     override suspend fun getDetails(movieId: Int): Response<MovieDetailsResponseVO> {
         return service.getMovieDetail(movieId)
+    }
+
+    override fun addMovieToFavorites(movie: MovieDetail): Boolean {
+        return storage.addMovie(movie)
+    }
+
+    override fun removeMovieToFavorites(movie: MovieDetail): Boolean {
+        return storage.removeMovie(movie)
+    }
+
+    override fun hasMovie(movie: MovieDetail): Boolean {
+        return storage.hasMovie(movie)
     }
 }
