7b7851142102505b8c820916d00bdeece1a9351f
louix.sm@gmail.com <Louiixx-h>
2023-04-20 21:06:01 -0300

    home refactored

--- app/src/main/java/br/com/luishenrique/moviesbrasil/home/HomeFragmentViewModel.kt
+++ app/src/main/java/br/com/luishenrique/moviesbrasil/home/HomeFragmentViewModel.kt
@@ -2,12 +2,14 @@ package br.com.luishenrique.moviesbrasil.home
 
 import androidx.lifecycle.LiveData
 import br.com.luishenrique.moviesbrasil.home.models.ResultMovie
+import br.com.luishenrique.moviesbrasil.home.models.responses.ResultMovieResponseVO
 
 interface HomeFragmentViewModel {
-    val progressBar: LiveData<Boolean>
-    val movieFromSearch: LiveData<ResultMovie>
-    val moviePopularList: LiveData<ResultMovie>
+    val command: LiveData<ResourceHome<ResultMovie>>
 
     fun getMovies()
     fun searchMovie(title: String)
+    fun onSuccessGetMovies(res: ResourceHome<ResultMovieResponseVO>)
+    fun onErrorGetMovies(exception: Exception)
+    fun onLoading(value: Boolean)
 }
