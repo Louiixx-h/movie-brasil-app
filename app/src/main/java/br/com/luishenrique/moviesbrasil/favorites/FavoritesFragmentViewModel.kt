c0422ec3a3e494f8e0aeee59c09204fb9744e2f6
louix.sm@gmail.com <Louiixx-h>
2023-04-20 21:06:18 -0300

    favorites refactored

--- app/src/main/java/br/com/luishenrique/moviesbrasil/favorites/FavoritesFragmentViewModel.kt
+++ app/src/main/java/br/com/luishenrique/moviesbrasil/favorites/FavoritesFragmentViewModel.kt
@@ -4,7 +4,10 @@ import androidx.lifecycle.LiveData
 import br.com.luishenrique.moviesbrasil.details.models.MovieDetail
 
 interface FavoritesFragmentViewModel {
+    val command: LiveData<ResourceFavorites<List<MovieDetail>>>
+
+    fun onSuccessGetMovies(response: ResourceFavorites<List<MovieDetail>>)
+    fun onErrorGetMovie(exception: Exception)
+    fun onLoading(value: Boolean)
     fun getMovies()
-    val movies: LiveData<List<MovieDetail>>
-    val progressBar: LiveData<Boolean>
 }
--- app/src/main/java/br/com/luishenrique/moviesbrasil/favorites/FavoritesFragmentViewModelImpl.kt
+++ app/src/main/java/br/com/luishenrique/moviesbrasil/favorites/FavoritesFragmentViewModelImpl.kt
@@ -2,36 +2,45 @@ package br.com.luishenrique.moviesbrasil.favorites
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import br.com.luishenrique.moviesbrasil.common.BaseViewModel
 import br.com.luishenrique.moviesbrasil.details.models.MovieDetail
 import br.com.luishenrique.moviesbrasil.favorites.repository.FavoritesRepository
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 class FavoritesFragmentViewModelImpl(
     private val repository: FavoritesRepository
-): ViewModel(), FavoritesFragmentViewModel {
-
-    private val _progressBar = MutableLiveData<Boolean>()
-    override val progressBar: LiveData<Boolean> = _progressBar
-
-    private val _movies = MutableLiveData<List<MovieDetail>>()
-    override val movies: LiveData<List<MovieDetail>> = _movies
+): BaseViewModel(), FavoritesFragmentViewModel {
+    
+    private val _command = MutableLiveData<ResourceFavorites<List<MovieDetail>>>()
+    override val command: LiveData<ResourceFavorites<List<MovieDetail>>> = _command
 
     override fun getMovies() {
-        _progressBar.postValue(true)
-        viewModelScope.launch(Dispatchers.IO) {
-            withContext(Dispatchers.Main) {
-                val response = repository.getMovies()
+        onLoading(true)
 
-                if (response.isNotEmpty()) {
-                    _movies.value = response
-                }
-
-                _progressBar.postValue(false)
-            }
+        viewModelScope.launch {
+            network.call(
+                block = { repository.getMovies() },
+                onSuccess = { onSuccessGetMovies(ResourceFavorites.Success(it)) },
+                onError = { onErrorGetMovie(it) },
+                finally = { onLoading(false) }
+            )
         }
+    }
+
+    override fun onSuccessGetMovies(response: ResourceFavorites<List<MovieDetail>>) {
+        if(response.data == null) {
+            onErrorGetMovie(Exception("Lista vazia!"))
+        }
+
+        _command.postValue(ResourceFavorites.Success(response.data!!))
+    }
+
+    override fun onErrorGetMovie(exception: Exception) {
+        _command.postValue(ResourceFavorites.Error(exception.message.orEmpty()))
+    }
+
+    override fun onLoading(value: Boolean) {
+        _command.postValue(ResourceFavorites.Loading(value))
     }
 }
